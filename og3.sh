e9a9133a9058d3267d8e584ad4bd9579573b1de9
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-08 00:13:45 +0800

    update og3

--- og3.sh
+++ og3.sh
@@ -32,8 +32,8 @@ git pull
 $JAVA_HOME/bin/java -Djava.util.logging.config.file=/workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/etc/logging.properties -jar /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/opengrok.jar -c /workspace/xulun/github/ctags/ctags -s /workspace/xulun/kernel/linux -d /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/data_kernel -H -P -S -G -W /workspace/xulun/opengrok/etc/kernel/configuration.xml -T 64 -v
 opengrok-deploy -c /workspace/xulun/opengrok/etc/kernel/configuration.xml /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/source.war /root/apache-tomcat-9.0.30/webapps/linux.war
 
-# /workspace/xulun/github/lang
+# /workspace/xulun/github/lang/lang
 
-$JAVA_HOME/bin/java -Djava.util.logging.config.file=/workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/etc/logging.properties -jar /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/opengrok.jar -c /workspace/xulun/github/ctags/ctags -s /workspace/xulun/github/lang -d /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/data_lang -H -P -S -G -W /workspace/xulun/opengrok/etc/lang/configuration.xml -T 64 -v
+$JAVA_HOME/bin/java -Djava.util.logging.config.file=/workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/etc/logging.properties -jar /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/opengrok.jar -c /workspace/xulun/github/ctags/ctags -s /workspace/xulun/github/lang/lang -d /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/data_lang -H -P -S -G -W /workspace/xulun/opengrok/etc/lang/configuration.xml -T 64 -v
 opengrok-deploy -c /workspace/xulun/opengrok/etc/lang/configuration.xml /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/source.war /root/apache-tomcat-9.0.30/webapps/lang.war
 
