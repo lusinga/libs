90e4f0aa7e4f0bca020e8246a80462d22b2533f8
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-07 17:52:09 +0800

    update og3

--- og3.sh
+++ og3.sh
@@ -3,3 +3,9 @@
 $JAVA_HOME/bin/java -Djava.util.logging.config.file=/workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/etc/logging.properties -jar /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/opengrok.jar -c /workspace/xulun/github/ctags/ctags -s /workspace/xulun/github/ros2 -d /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/data_ros2 -H -P -S -G -W /workspace/xulun/opengrok/etc/ros2/configuration.xml -v
 
 opengrok-deploy -c /workspace/xulun/opengrok/etc/ros2/configuration.xml /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/source.war /root/apache-tomcat-9.0.30/webapps/ros2.war
+
+# /workspace/xulun/github/ai
+
+$JAVA_HOME/bin/java -Djava.util.logging.config.file=/workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/etc/logging.properties -jar /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/opengrok.jar -c /workspace/xulun/github/ctags/ctags -s /workspace/xulun/github/ai -d /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/data_ai -H -P -S -G -W /workspace/xulun/opengrok/etc/ai/configuration.xml -v
+
+opengrok-deploy -c /workspace/xulun/opengrok/etc/ai/configuration.xml /workspace/xulun/gitlab/opengrok/distribution/target/opengrok-1.3.6/lib/source.war /root/apache-tomcat-9.0.30/webapps/ai.war
