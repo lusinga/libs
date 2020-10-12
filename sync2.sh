c25f8d1f7f99b40c7ebf5677a99b6497c6da8d1d
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-12 14:19:18 +0800

    update sync2.sh

---
+++ sync2.sh
@@ -0,0 +1,18 @@
+cd /workspace/xulun/alios/AS22L_lite_pbase_ivi_mtk2712/
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/AS22L_lite_venus_dev/
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/BMV1_lite_venus_dev/
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/AS22L_baseos_dev/
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/AS22_lite_venus_dev_zama
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/pbase_ivi_rcarh3n
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/AS22L_baseos_dev_gearv/
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/SWORD-1_lite_venus_dev/
+./repo sync -j64 --force-sync
+cd /workspace/xulun/alios/AS22L_baseos_dev_gearv/
+./repo sync -j64 --force-sync
