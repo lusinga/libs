c9215b242eb0c0afb7a9d715988a0b23aff84016
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-12 16:24:17 +0800

    update score

--- testscore.c
+++ testscore.c
@@ -1,21 +1,36 @@
 #include "klee/klee.h"
 
-char testscore(int score){
-    if(score>100 || score <0){
+char testscore(int score)
+{
+    if (score > 100)
+    {
         return 'E';
-    }else if(score>=90){
+    }
+    else if (score < 0)
+    {
+        return 'E';
+    }
+    else if (score >= 90)
+    {
         return 'A';
-    }else if(score>=80){
+    }
+    else if (score >= 80)
+    {
         return 'B';
-    }else if(score>=60){
+    }
+    else if (score >= 60)
+    {
         return 'C';
-    }else{
+    }
+    else
+    {
         return 'D';
     }
 }
 
-int main(){
+int main()
+{
     int score;
-    klee_make_symbolic(&score,sizeof(score),"score");
-    return(testscore(score));
+    klee_make_symbolic(&score, sizeof(score), "score");
+    return (testscore(score));
 }
