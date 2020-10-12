6c350347629cdde03dce24a54f47f29155489993
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-12 15:03:08 +0800

    update testscore

---
+++ testscore.c
@@ -0,0 +1,19 @@
+char testscore(int score){
+    if(score>100 || score <0){
+        return 'E';
+    }else if(score>=90){
+        return 'A';
+    }else if(score>=80){
+        return 'B';
+    }else if(score>=60){
+        return 'C';
+    }else{
+        return 'D';
+    }
+}
+
+int main(){
+    int score;
+    klee_make_symbolic(&score,sizeof(score),"score");
+    return(testscore(score));
+}
