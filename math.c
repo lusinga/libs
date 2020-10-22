b7398d62484f62bba9b45c39b0e47c42ce36e3b7
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-22 19:11:38 +0800

    update sqrt

--- math.c
+++ math.c
@@ -10,9 +10,22 @@ int sqrt2(int n){
     }
 }
 
+int sqrt3(float n){
+    if(n<0.0){
+        return 0;
+    }else if(sqrt(n)<100){
+        return (int)sqrt(n);
+    }else{
+        return 100;
+    }
+}
+
 int main(){
     //printf("fib %d=%d\n",4, fib(4));
     int a;
     klee_make_symbolic(&a,sizeof(a),"a");
+    float f;
+    klee_make_symbolic(%f,sizeof(f),"f");
+    sqrt3(f);
     return(sqrt2(a));
 }
