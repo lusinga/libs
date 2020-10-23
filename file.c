9298e75f65761d58dfef336e737b5b8abcd5c97a
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-23 21:45:00 +0800

    update

--- file.c
+++ file.c
@@ -6,17 +6,20 @@
 #include "klee/klee.h"
 
 int fileo(int n){
+    if(n<0){
+        return -1;
+    }
     FILE* fp = fopen("data.dat","r+");
     if(fp!=NULL){
         if(n>10){
-            return -1;
+            return -2;
         }
         char data[10];
         int nCount = fread(data,1,n,fp);
         fclose(fp);
         return nCount;
     }else{
-        return -2;
+        return -3;
     }
 }
 
