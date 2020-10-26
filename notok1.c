aeec7cf79c8d402916d15dde583d60bfeec96fcc
xulun <ziying.liuziying@alibaba-inc.com>
2020-10-26 17:11:24 +0800

    update notok

---
+++ notok1.c
@@ -0,0 +1,25 @@
+//
+// Created by lusinga on 2020/10/26.
+//
+
+#include "klee/klee.h"
+
+char sign2(char a){
+    if(a>0){
+        if(a<-10){
+            return -2;
+        }else{
+            return 1;
+        }
+    }else if(a==0){
+        return 0;
+    }else{
+        return -1;
+    }
+}
+
+int main(void){
+    char a;
+    klee_make_symbolic(&a,sizeof(a),"a");
+    return sign2(a);
+}
